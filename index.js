9e87d75ffe4d7fbcf47689cb906814510fa91c69
Your Name <you@example.com>
2020-07-08 00:29:50 +0300

    navbar fixed on all sizes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(5555);
 
-console.log((__dirname+'/index.html'));
 
 app.get('/', (req, res) => {
     res.sendFile((__dirname+'/index.html'));
